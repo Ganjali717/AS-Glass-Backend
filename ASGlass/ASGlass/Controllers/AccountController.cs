a214af45dd45371136c57ec8b8a44d8b293ea313
Ganjali717 <ganjaliki@code.edu.az>
2021-11-21 22:52:09 +0400

    All contollers is ready. Some models created

---
+++ ASGlass/ASGlass/Controllers/AccountController.cs
@@ -0,0 +1,28 @@
+﻿using ASGlass.DAL;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASGlass.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public AccountController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult LogIn()
+        {
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
